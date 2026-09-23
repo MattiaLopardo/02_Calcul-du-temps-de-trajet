d8d9c94bb7897e30666472935594551bf1a535c2
mattia <mattialopardo@gmail.com>
2026-09-23 16:49:53 +0200

    La première version du code est fonctionnelle.

--- main.cpp
+++ main.cpp
@@ -15,24 +15,36 @@ int main () {
     int dy = 0;
     int dx = 0;
     int L1 = 0;
-    int v1 = 5;
-    int v2 = 2;
+    int s1 = 5;
+    int s2 = 2;
 
 
-    cout << "veuillez entrer ci-dessous la valeur de dy :" << endl;
+    cout << "veuillez entrer ci-dessous en km la longueur total de la route (variable = dy) :" << endl;
     cin >> dy;
-    cout << "veuillez entrer ci-dessous la valeur de dx :" << endl;
+    cout << "veuillez entrer ci-dessous en km la longueur du segment sur terre (variable = dx) :" << endl;
     cin >> dx;
-    cout << "veuillez entrer ci-dessous la valeur de L1 :" << endl;
+    cout << "veuillez entrer ci-dessous en km la longueur de la route parcouru par le robot (variable = L1) :" << endl;
     cin >> L1;
-    bool valeurdentree = ((dy>0) && (dx>0) && (L1>0));
-    
-    /*
-    int L3 = dy-L1;
-    int L2exp = (dx * dx) + (L3 * L3);
-    int L2 = sqrt(L2exp);
-    cout << L2;
-/*
-    cout << "le temps employé par le robot est de " L1/v1 +
-*/
+    cout << "veuillez entrer ci-dessous en km/h la vitesse du robot sur route (variable = s1) :" << endl;
+    cin >> s1;
+    cout << "veuillez en fin entrer ci-dessous en km/h la vitesse du robot sur terre (variable = s2) :" << endl;
+    cin >> s2;
+
+    bool valeurdentree = ((dy>0) && (dx>0) && (L1>0) && (s1>0) && (s2>0)); // vérification des valeurs en entrée
+
+    if (valeurdentree==false) {
+        cout << "les valeurs en entree doivent toutes etre plus grandes que 0" << endl;
+        return EXIT_FAILURE;
+    }
+
+    if (valeurdentree==true) {
+
+        int L3 = dy-L1;
+        int L2 = sqrt((dx * dx) + (L3 * L3));
+        int theure = L1/s1 + L2/s2;
+
+        cout << "le robot mettra " << theure << " heures pour atteindre son objectif" << endl;
+        return EXIT_SUCCESS;
+    }
+
 }
