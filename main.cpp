6388197d08bc063bedd9f4bd2a074f47eb295ba0
mattia <mattialopardo@gmail.com>
2026-09-23 14:31:15 +0200

    pseudo code ok. Travail sur le code

--- main.cpp
+++ main.cpp
@@ -5,3 +5,34 @@ Date :
 But : Calcul du temps de trajet 
 Remarque(s) : 
 --------------------------- */
+#include <cmath>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main () {
+    int dy = 0;
+    int dx = 0;
+    int L1 = 0;
+    int v1 = 5;
+    int v2 = 2;
+
+
+    cout << "veuillez entrer ci-dessous la valeur de dy :" << endl;
+    cin >> dy;
+    cout << "veuillez entrer ci-dessous la valeur de dx :" << endl;
+    cin >> dx;
+    cout << "veuillez entrer ci-dessous la valeur de L1 :" << endl;
+    cin >> L1;
+    bool valeurdentree = ((dy>0) && (dx>0) && (L1>0));
+    
+    /*
+    int L3 = dy-L1;
+    int L2exp = (dx * dx) + (L3 * L3);
+    int L2 = sqrt(L2exp);
+    cout << L2;
+/*
+    cout << "le temps employé par le robot est de " L1/v1 +
+*/
+}
--- pseudo.md
+++ pseudo.md
@@ -1,1 +1,24 @@
-# Pseudo code de votre programme
+/*
+Pseudo code:
+
+1) Demande d'entrée des distances
+   11. Demande de la distance dy
+   12. Demande de la distance dx
+   13. Demande de la distance L1
+
+2) Entrée des distance par l'utilisateur
+
+3) Vérification des valeurs d'entrée
+   31. Vérification que dy n'est pas égal a 0 
+   32. Vérification que dx n'est pas égal a 0 
+   33. Vérification que L1 n'est pas égal a 0
+
+4) Si une valeur est égal a 0 ne pas exécuter la suite et arrêter le programme
+
+5) Calcul du temps de déplacement du robot 
+   51. Calculer la valeur de L2 
+   52. calculer le temps total, soit : L1/v1 + L2/v2 = t
+6) Stockage de la réponse final
+7) Affichage de la réponse à l'utilisateur
+8) Fin du programme avec la valeur EXIT_SUCCESS
+*/
